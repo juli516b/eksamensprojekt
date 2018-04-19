8c83a28dc0a5e40a15fea5556370ecf5ae600203
Julian Mathias Kock <juli516b@edu.eal.dk>
2018-04-19 12:50:04 +0200

    Implementeret klasserne Offer, OfferLine og OfferViewModel og dertilhørende metoder og properties.

---
+++ Eksamensprojekt/Model/Offer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Offer
+    {
+        private string offerNo;
+        private DateTime offerDate;
+        private IList<OfferLine> offerLines;
+        private double offerTotal;
+
+        public string OfferNo
+        {
+            get { return offerNo; }
+            set { offerNo = value; }
+        }
+
+        public DateTime OfferDate
+        {
+            get { return offerDate; }
+            set { offerDate = value; }
+        }
+
+        public IList<OfferLine> OfferLines
+        {
+            get { return offerLines; }
+            set { offerLines = value; }
+        }
+
+        public double OfferTotal
+        {
+            get { return offerTotal; }
+            set { offerTotal = value; }
+        }
+
+        public Offer(DateTime creationDate)
+        {
+            OfferDate = creationDate;
+        }
+
+        public void AddOfferLine(OfferLine offerLine)   
+        {
+            OfferLines.Add(offerLine);
+        }
+
+
+
+    }
+}
---
+++ Eksamensprojekt/ViewModel/OfferViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ViewModel
+{
+    class OfferViewModel
+    {
+        public Offer ThisOffer { get; set; }
+        public IList<IItem> Items { get; set; }
+
+        public Offer CreateOffer(DateTime creationDate)
+        {
+            Offer newOffer = new Offer(creationDate);
+            return newOffer;
+        }
+        public void AddOfferLine(Offer thisOffer, IItem item, int quantity)
+        {
+            OfferLine newOfferLine = new OfferLine(item, quantity);
+            thisOffer.AddOfferLine(newOfferLine);
+        }
+    }
+}
