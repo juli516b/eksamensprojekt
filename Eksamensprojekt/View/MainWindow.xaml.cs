6e0cd392609dc5f19713f44d225cefb8b7374238
larzraz <lars5030@edu.eal.dk>
2018-04-19 10:50:50 +0200

    Flytter startup til View og flyttet gui fra Views til View og slettet Views

--- Eksamensprojekt/View/MainWindow.xaml.cs
+++ Eksamensprojekt/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ViewModel;
 
 namespace Eksamensprojekt
 {
@@ -23,6 +24,7 @@ namespace Eksamensprojekt
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = ViewModel.ViewModel.GetInstance();
         }
     }
 }
