65f70e32c5aa8d278786222f2ae67e702e7f8357
Julian Mathias Kock <juli516b@edu.eal.dk>
2018-04-24 14:01:37 +0200

    Fixede lige testene engang til.

--- Eksamensprojekt/PomfritTest/OfferLineTest.cs
+++ Eksamensprojekt/PomfritTest/OfferLineTest.cs
@@ -8,7 +8,7 @@ namespace RabatManOfferLineDiscountTest
     public class OfferLineTest
     {
         [TestMethod]
-        public void DiscountedPriceMathTest()
+        public void OfferLineTotalTest()
         {
             //Arrange
             IBaseItem item = new Item("Kyllinge vinger", "123", 20.25);
@@ -16,7 +16,16 @@ namespace RabatManOfferLineDiscountTest
             //Act
             //Assert
             Assert.AreEqual(2025, offerLine.OfferLineTotal);
+        }
+        [TestMethod]
+        public void DiscountedPriceMathTest()
+        {
+            //Arrange
+            IBaseItem item = new Item("Kyllinge vinger", "123", 20.25);
+            OfferLine offerLine = new OfferLine(item, 100);
+            //Act
             offerLine.DiscountedPrice = 15;
+            //Assert
             Assert.AreEqual(1500, offerLine.OfferLineTotal);
             Assert.AreEqual(25.9259259259, offerLine.PercentDiscount, 5);
         }
@@ -27,9 +36,8 @@ namespace RabatManOfferLineDiscountTest
             IBaseItem item = new Item("Kyllinge vinger", "123", 20.25);
             OfferLine offerLine = new OfferLine(item, 100);
             //Act
-            //Assert
-            Assert.AreEqual(2025, offerLine.OfferLineTotal);
             offerLine.PercentDiscount = 20;
+            //Assert
             Assert.AreEqual(1620, offerLine.OfferLineTotal);
             Assert.AreEqual(16.2, offerLine.DiscountedPrice);
         }
