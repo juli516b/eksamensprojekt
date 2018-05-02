f2e02ef71d86e27ae39ef5a8fa1f5c74455171f3
jesp209i <jesp209i@edu.eal.dk>
2018-05-02 13:45:34 +0200

    customers tilføjet

---
+++ Eksamensprojekt/Model/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CustomerRepository
+    {
+        public IList<Customer> Customers { get; set; }
+        public CustomerRepository()
+        {
+            Customers = new List<Customer>();
+        }
+        public void AddCustomer(Customer newCustomer)
+        {
+            Customers.Add(newCustomer)
+        }
+    }
+}
