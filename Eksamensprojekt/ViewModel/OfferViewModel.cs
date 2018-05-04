a62700447c0d05d15614a67bcc48d61816de9750
Mathias Stubberup Nabe-Nielsen <mahh0001@edu.eal.dk>
2018-05-04 10:55:21 +0200

    Fixed logistic binding

--- Eksamensprojekt/ViewModel/OfferViewModel.cs
+++ Eksamensprojekt/ViewModel/OfferViewModel.cs
@@ -175,6 +175,8 @@ namespace ViewModel
             OfferLines.Add(newOfferLine);
             newOfferLine.APWC += NotifyPropertyChanged;
             NotifyPropertyChanged("OfferTotal");
+            NotifyPropertyChanged("NoOfTotalPackages");
+            NotifyPropertyChanged("NoOfTotalPallets");
         }
     }
 }
