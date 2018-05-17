49d64235013f4dfbad1c98e3d0aa1d309b65baf7
Julian Mathias Kock <juli516b@edu.eal.dk>
2018-05-17 11:31:22 +0200

    Rettet tabeller således der bliver genereret en ny side, hvis den overlapper

--- Eksamensprojekt/Model/PDFExporter.cs
+++ Eksamensprojekt/Model/PDFExporter.cs
@@ -10,7 +10,8 @@ namespace Model
     {
         private string SaveFileDialogWindow()
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog
+            SaveFileDialog saveFileDialog;
+            saveFileDialog = new SaveFileDialog
             {
                 InitialDirectory = @"C:\",
                 Title = "Select PDFFile",
@@ -19,36 +20,50 @@ namespace Model
                 FileName = DateTime.Now.ToShortDateString()
             };
             if (saveFileDialog.ShowDialog() == true)
-            {
                 return saveFileDialog.FileName;
-            }
-            else
-            {
-                return "Du skal fatte noget";
-            }
+            throw new Exception("Der er sket en fejl med at gemme filen");
         }
         public void PDFGenerator(Offer currentOffer)
         {
+            //INITIALISERE NEW DOCUMENT
             Document doc = new Document(PageSize.A4);
             PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(SaveFileDialogWindow(), FileMode.Create));
+            Chunk symbol = new Chunk("", FontFactory.GetFont("HELVETICA"));
             doc.Open();
+            Paragraph kundeParagraphListe = new Paragraph("KUNDE INFORMATION");
+            List kundeInformationListe = new List()
+            {
+                ListSymbol = symbol,
+                IndentationLeft = 30f
+            };
+            if (currentOffer.MyCustomer != null)
+            {
+                kundeInformationListe.Add(currentOffer.MyCustomer.CustomerName);
+                kundeInformationListe.Add(currentOffer.MyCustomer.CustomerAdress);
+                kundeInformationListe.Add(currentOffer.MyCustomer.CustomerZip + "");
+                kundeInformationListe.Add("CVR nummer: " + currentOffer.MyCustomer.CVRNumber);
+                kundeInformationListe.Add(currentOffer.MyCustomer.Email);
+            }
+            else
+            kundeInformationListe.Add("INGEN KUNDE VALGT");
 
-            PdfContentByte pcb = writer.DirectContent;
             //ECONOMY TABLE
-            PdfPTable economyTable = new PdfPTable(2);
-            economyTable.DefaultCell.Border = 0;
-            economyTable.DefaultCell.HorizontalAlignment = 2;
+            PdfPTable economyTable = new PdfPTable(2)
+            {
+                DefaultCell =
+                {
+                    Border = 0,
+                    HorizontalAlignment = 2
+                },
+                TotalWidth = 400,
+            };
             economyTable.AddCell("SUBTOTAL:");
             economyTable.AddCell(currentOffer.OfferSubtotal + " DKK");
             economyTable.AddCell("KUNDERABAT:");
-            if (currentOffer.MyCustomer != null)
-            {
-                economyTable.AddCell(currentOffer.MyCustomer.CustomerDiscount + " %");
-            }
-            else
-            {
+            if (currentOffer.MyCustomer == null)
                 economyTable.AddCell("0 %");
-            }
+            else
+                economyTable.AddCell(currentOffer.MyCustomer.CustomerDiscount + " %");
             economyTable.AddCell("TILBUDSRABAT");
             economyTable.AddCell(currentOffer.OfferDiscount + " %");
             economyTable.AddCell("TRANSPORTOMKOSTNINGER:");
@@ -59,37 +74,45 @@ namespace Model
             economyTable.AddCell(currentOffer.TotalDiscountedPrice);
             economyTable.AddCell("TOTAL:");
             economyTable.AddCell(currentOffer.OfferTotal + " DKK");
-            economyTable.TotalWidth = 400;
             //ECONOMY TABLE
             //OFFERLINES TABLE
-            PdfPTable offerlineTable = new PdfPTable(7);
-            offerlineTable.DefaultCell.Border = 0;
-            offerlineTable.DefaultCell.Padding = 4;
-            offerlineTable.DefaultCell.PaddingLeft = 4;
-            offerlineTable.DefaultCell.HorizontalAlignment = 2;
-            offerlineTable.WidthPercentage = 100;
-            offerlineTable.AddCell("Vare nr.");
-            offerlineTable.AddCell("Varenavn");
-            offerlineTable.AddCell("Antal varer");
-            offerlineTable.AddCell("Stk. pris, DKK");
-            offerlineTable.AddCell("Tilbudspris, DKK");
-            offerlineTable.AddCell("Rabat, %");
-            offerlineTable.AddCell("Total, DKK");
-            offerlineTable.HeaderRows = 1;
+            PdfPTable offerLineTable = new PdfPTable(7)
+            {
+                DefaultCell =
+                {
+                    Border = 0,
+                    Padding = 4,
+                    PaddingLeft = 4,
+                    HorizontalAlignment = 2
+                },
+                TotalWidth = 550,
+                HeaderRows = 1,
+                SplitLate = false
+            };
+            //MAKING HEADERROWS
+            offerLineTable.AddCell("Vare nr.");
+            offerLineTable.AddCell("Varenavn");
+            offerLineTable.AddCell("Antal varer");
+            offerLineTable.AddCell("Stk. pris, DKK");
+            offerLineTable.AddCell("Tilbudspris, DKK");
+            offerLineTable.AddCell("Rabat, %");
+            offerLineTable.AddCell("Total, DKK");
+            //ADDING OFFERLINE TO offerLineTable.
             foreach (OfferLine offerLine in currentOffer.OfferLines)
             {
-                offerlineTable.AddCell(offerLine.ItemNo);
-                offerlineTable.AddCell(offerLine.ItemName);
-                offerlineTable.AddCell(offerLine.Quantity + "");
-                offerlineTable.AddCell(offerLine.ItemPrice + "");
-                offerlineTable.AddCell(offerLine.DiscountedPrice + "");
-                offerlineTable.AddCell(offerLine.PercentDiscount + "");
-                offerlineTable.AddCell(offerLine.OfferLineTotal + "");
+                offerLineTable.AddCell(offerLine.ItemNo);
+                offerLineTable.AddCell(offerLine.ItemName);
+                offerLineTable.AddCell(offerLine.Quantity + "");
+                offerLineTable.AddCell(offerLine.ItemPrice + "");
+                offerLineTable.AddCell(offerLine.DiscountedPrice + "");
+                offerLineTable.AddCell(offerLine.PercentDiscount + "");
+                offerLineTable.AddCell(offerLine.OfferLineTotal + "");
             }
-            //OFFERLINES TABLE
             //ADD TO DOCUMENT
-            doc.Add(offerlineTable);
-            economyTable.WriteSelectedRows(0, -1, 150, 150, pcb);
+            doc.Add(kundeParagraphListe);
+            doc.Add(kundeInformationListe);
+            doc.Add(offerLineTable);
+            doc.Add(economyTable);
             doc.Close();
         }
     }
