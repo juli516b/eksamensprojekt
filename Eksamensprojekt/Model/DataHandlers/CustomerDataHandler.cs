985d6642945ec5624709834e84216698ec054f4b
jesp209i <jesp209i@edu.eal.dk>
2018-05-23 00:18:38 +0200

    fixed interface, model and fakedata

--- Eksamensprojekt/Model/DataHandlers/CustomerDataHandler.cs
+++ Eksamensprojekt/Model/DataHandlers/CustomerDataHandler.cs
@@ -13,36 +13,39 @@ namespace Model.DataHandlers
                 CustomerAdress = "Norgesvej 17",
                 CustomerDiscount = 3,
                 CustomerZip = 5000,
+                CustomerCity = "Odense",
                 CVRNumber = "$@!§&&",
                 Email = "Britta@Ikrea.se.dk",
                 PhoneNo = 12345678,
                 CustomerCountry = "Sverige"
-            }
+                }
             );
             customers.Add(new Customer
                 {
                 CustomerName = "Pommes Konkurrent IPS",
                 CustomerAdress = "Norgesvej 18",
                 CustomerDiscount = 1,
-                CustomerZip = 5003,
+                CustomerZip = 2860,
+                CustomerCity =  "Søborg",
                 CVRNumber = "8dk",
                 Email = "nulle@pommes.dk",
                 PhoneNo = 23456789,
                 CustomerCountry = "Jylland"
-            }
-);
+                }
+            );
             customers.Add(new Customer
                 {
                 CustomerName = "Grøntsager APS",
                 CustomerAdress = "Norgesvej 19",
                 CustomerDiscount = 9,
-                CustomerZip = 9000,
+                CustomerZip = 90210,
+                CustomerCity = "Beverly Hills",
                 CVRNumber = "6",
                 Email = "Gert@greens.dk",
                 PhoneNo = 34567890,
                 CustomerCountry = "Danmark"
-            }
-);
+                }
+            );
             return customers;
         }
     }
