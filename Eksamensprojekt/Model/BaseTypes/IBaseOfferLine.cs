cf43b14de55222813bd6f81a565f45efca3e9c79
jesp209i <jesp209i@edu.eal.dk>
2018-05-25 10:25:21 +0200

    rettelser

--- Eksamensprojekt/Model/BaseTypes/IBaseOfferLine.cs
+++ Eksamensprojekt/Model/BaseTypes/IBaseOfferLine.cs
@@ -3,6 +3,7 @@
     public interface IBaseOfferLine
     {
         int OfferLineId { get; set; }
+        IBaseItem Item { get; set; }
         int Quantity { get; set; }
         double DiscountedPrice { get; set; }
     }
