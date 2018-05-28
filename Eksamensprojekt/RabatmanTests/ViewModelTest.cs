ddaaca1088d00c789f5b33b51b8201db48f5ffb0
Mathias Stubberup Nabe-Nielsen <mahh0001@edu.eal.dk>
2018-05-28 11:21:52 +0200

    Rettet kommentar i ViewModelTest

--- Eksamensprojekt/RabatmanTests/ViewModelTest.cs
+++ Eksamensprojekt/RabatmanTests/ViewModelTest.cs
@@ -21,22 +21,20 @@ namespace RabatmanTests
             IBaseItem item = new Item("PommesDeluxe", "12345678", 1, 25);
             IBaseItem item2 = new Item("PommesDeluxe", "12345678", 2.50, 25); 
 
-            Assert.AreEqual(0 + " DKK", novm.OfferTotal); // no OfferLines added
-            novm.AddOfferLine(item, 100);
-            Assert.AreEqual(100 + " DKK", novm.OfferTotal); // one offerline added : OfferTotal = 100
-            novm.OfferDiscountPercent = 20;
             //Act
-
-            Assert.AreEqual(80 + " DKK", novm.OfferTotal); // OfferDiscount set to 20 (%) : OfferTotal = 80
+            //Assert
+            Assert.AreEqual(0 + " DKK", novm.OfferTotal); //No offerLines added
+            novm.AddOfferLine(item, 100);
+            Assert.AreEqual(100 + " DKK", novm.OfferTotal); //One offerline added and OfferTotal = 100
+            novm.OfferDiscountPercent = 20;
+            Assert.AreEqual(80 + " DKK", novm.OfferTotal); //OfferDiscount set to 20 (%): OfferTotal = 80
             novm.AddOfferLine(item2,4);
-            Assert.AreEqual(88 + " DKK", novm.OfferTotal); // one more offerline added : OfferTotal = (100 + 10) *0,8 = 88 
+            Assert.AreEqual(88 + " DKK", novm.OfferTotal); //Another offerline added: OfferTotal = (100 + 10) *0,8 = 88 
             novm.OfferDiscountPercent = 10;
-            Assert.AreEqual(99 + " DKK", novm.OfferTotal); // OfferDiscount corrected to 10 (%) : OfferTotal = (100 + 10) * 0,9 = 99
-
+            Assert.AreEqual(99 + " DKK", novm.OfferTotal); //OfferDiscount corrected to 10 (%): OfferTotal = (100 + 10) * 0,9 = 99
             novm.AddOfferLine(item,100);
             novm.OfferLines[2].PercentDiscount = 20;
-            Assert.AreEqual(171 + " DKK", novm.OfferTotal); // one more offerline added and that lines discount set to 20
-            // OfferTotal = (100 + 10 + 100 * 0,8) * 0,9 = 171
+            Assert.AreEqual(171 + " DKK", novm.OfferTotal); //Yet another offerline added with discount set to 20: OfferTotal = (100 + 10 + 100 * 0,8) * 0,9 = 171
 
         }
         [TestMethod]
@@ -53,7 +51,6 @@ namespace RabatmanTests
             //Assert
             Assert.AreEqual(2, ovm.NoOfTotalPallets);
             Assert.AreEqual(7, ovm.NoOfTotalPackages);
-
             ovm.AddOfferLine(item2, 31);
             Assert.AreEqual(3, ovm.NoOfTotalPallets);
             Assert.AreEqual(13, ovm.NoOfTotalPackages);
